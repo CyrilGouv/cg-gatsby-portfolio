f56354fd1ac782885654bcb7480da55c860d3489
Cyril Gouverneur <cyril@wifibridge.home>
2019-04-16 10:44:55 +0200

    Correcting Footer Mistake

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import Header from "./Home/header"
-import Footer from "./Base/footer"
+import Footer from "./Base/Footer"
 import Cursor from '../components/Base/Cursor'
 import "../scss/main.scss"
 
