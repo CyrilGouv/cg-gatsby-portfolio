2735719aaacab99819efeae19324b2fb558991dc
Cyril Gouverneur <cyril@wifibridge.home>
2019-04-19 12:13:33 +0200

    Add ScrollMagic with Gsap

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,52 +1,128 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link, graphql, StaticQuery } from "gatsby"
 import Img from 'gatsby-image'
+import { TimelineLite, Power1, Power2 } from "gsap/TweenMax";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
+import ScrollMagic from '../Base/ScrollMagic';
+
+
 
 import arrowImg from '../../images/base/arrow-black.svg'
 
-const About = () => (
-    <section className="homeAbout">
-        <div className="homeAbout-blurb">
-            <h4>Design. Code.</h4>
-            <h4>Technologie.</h4>
-            <p>Je suis Cyril.</p>
-            <p>Développeur front-end, Intégrateur Web & Webdesigner freelance basé actuellement dans le sud-ouest de la France à Biscarrosse dans le département des Landes.</p>
-            <p>Passionné et curieux, je mets dans chaque projet toute ma passion pour le numérique afin de créer des applications web uniques adaptés à vos besoins.</p>
-            <Link to="/a-propos" className="btn--default">
-                En savoir plus
-                <img src={ arrowImg } alt="Flèche appel à l'action vers à propos"/>    
-            </Link>
-        </div>
-        
-        <StaticQuery
-            query={ Image }
-            render={ data => {
-                const aboutProto = data.proto.childImageSharp.fluid
-                const aboutDesign = data.design.childImageSharp.fluid
 
-                return (
-                    <div className="homeAbout-img">
-                        <figure className="homeAbout-img--proto">
-                            <Img fluid={ aboutProto } />
-                        </figure>
-                        <figure className="homeAbout-img--design">
-                            <Img fluid={ aboutDesign } />
-                        </figure>
-                    </div>
-                )
-            } }
-        />
+export default class About extends Component {
 
-        <div className="about-transform">
-            <p>
-                Transformons vos idées en réalité.<br />
-                <Link to="/contact">Parlons-en maintenant !</Link>
-            </p>
-        </div>
+    componentDidMount = () => {
+        // Init Scroll Magic
+        const controller = new ScrollMagic.Controller()
         
-    </section>
-)
-export default About
+        // Init Timeline
+        const scrollAboutTl = new TimelineLite()
+        const scrollAboutImgTl = new TimelineLite()
+
+        // Vars
+        const homeAboutFl = this.refs.homeAboutFl
+        const homeAboutSl = this.refs.homeAboutSl
+        const homeAboutPara1 = this.refs.homeAboutPara1
+        const homeAboutPara2 = this.refs.homeAboutPara2
+        const homeAboutPara3 = this.refs.homeAboutPara3
+        const rule = CSSRulePlugin.getRule(".btn--default::before")
+        // const homeAboutImgProto = this.refs.homeAboutImgProto
+        // const homeAboutImgDesign = this.refs.homeAboutImgDesign
+
+        // Trigger Elements
+        const triggerAboutBlurb = this.refs.triggerAboutBlurb
+        // const homeAboutImg = this.refs.homeAboutImg
+        
+
+        // About Title Scene
+        const revealAboutBlurb = new ScrollMagic.Scene({
+            triggerElement: triggerAboutBlurb,
+            triggerHook: 0.5,
+            reverse: false
+        })
+        .setTween(
+            scrollAboutTl
+                .from(homeAboutFl, .85, { transform: 'translateY(100%)', ease: Power2.easeOut })
+                .from(homeAboutSl, .85, { transform: 'translateY(100%)', ease: Power2.easeOut }, '-=0.45')
+                .from(homeAboutPara1, .5, { autoAlpha: 0, y: 30, ease: Power1.easeOut })
+                .from(homeAboutPara2, .5, { autoAlpha: 0, y: 30, ease: Power1.easeOut }, '-=.15')
+                .from(homeAboutPara3, .5, { autoAlpha: 0, y: 30, ease: Power1.easeOut }, '-=.15')
+                .from('.homeAbout-blurb .btn--default', 1, { autoAlpha: 0, y: 30, ease: Power1.easeOut })
+                .from('.homeAbout-blurb .btn--default', .5, { paddingLeft: 0, ease: Power1.easeOut })
+                .from(rule, .25, { cssRule: { width: 0 }, ease: Power1.easeOut }, '-=0.5')
+        )
+        .addTo(controller)
+
+        // About Img Scene
+        const revealAboutImg = new ScrollMagic.Scene({
+            triggerElement: '.homeAbout-img',
+            triggerHook: .65,
+            reverse: false
+        })
+        .setTween(
+            scrollAboutImgTl
+                .staggerFrom('.reveal-about--mask', .85, { transform: 'translateY(100%)', ease: Power2.easeOut }, 0.35)
+        )
+        .addTo(controller)
+    }
+    
+
+    render() {
+        return (
+            <section className="homeAbout">
+                <div className="homeAbout-blurb" ref="triggerAboutBlurb">
+                    <h4>
+                        <span ref="homeAboutFl">
+                            Design. Code.
+                        </span>
+                    </h4>
+                    <h4>
+                        <span ref="homeAboutSl">
+                            Technologie.
+                        </span>        
+                    </h4>
+                    <p ref="homeAboutPara1">Je suis Cyril.</p>
+                    <p ref="homeAboutPara2">Développeur front-end, Intégrateur Web & Webdesigner freelance basé actuellement dans le sud-ouest de la France à Biscarrosse dans le département des Landes.</p>
+                    <p ref="homeAboutPara3">Passionné et curieux, je mets dans chaque projet toute ma passion pour le numérique afin de créer des applications web uniques adaptés à vos besoins.</p>
+                    <Link to="/a-propos" className="btn--default">
+                        En savoir plus
+                        <img src={ arrowImg } alt="Flèche appel à l'action vers à propos"/>    
+                    </Link>
+                </div>
+                
+                <StaticQuery
+                    query={ Image }
+                    render={ data => {
+                        const aboutProto = data.proto.childImageSharp.fluid
+                        const aboutDesign = data.design.childImageSharp.fluid
+
+                        return (
+                            <div className="homeAbout-img">
+                                <figure className="homeAbout-img--proto">
+                                    <Img fluid={ aboutProto } />
+                                    <div className="reveal-about--mask"></div>
+                                </figure>
+                                <figure className="homeAbout-img--design">
+                                    <Img fluid={ aboutDesign } />
+                                    <div className="reveal-about--mask"></div>
+                                </figure>
+                            </div>
+                        )
+                    } }
+                />
+
+                <div className="about-transform">
+                    <p>
+                        Transformons vos idées en réalité.<br />
+                        <Link to="/contact">Parlons-en maintenant !</Link>
+                    </p>
+                </div>
+                
+            </section>
+        )
+    }
+}
 
 
 const Image = graphql`
